5810e10d9468eed695714d997604d926bb0d0c8d
Jose Ribeiro dos Santos Neto <jose.santos@Workable-SP.com>
2025-12-30 16:02:54 -0300

    Criando o backend da aplicação

---
+++ backend/src/types/Investigation.ts
@@ -0,0 +1,60 @@
+// DTO
+export interface ProcessDTO {
+    id: number;                 
+    requestname: string;
+    setor: string;              
+    requester_setor: string;
+    requester_setor_id: number;
+    requester_cargo: string;
+    requester_cargo_id: number;
+    flow_name: string;
+    task_instance_id: number;
+    task_id: number;
+    task_name: string;
+    task_type: string;
+    task_result: string;
+    task_startdatetime: string | Date; 
+    task_enddatetime: string | Date;
+    executor_userid: number;
+    executor_name: string;
+    executor_email: string;
+    executor_username: string;
+    reportlink: string;
+}
+
+export interface EmployeeEntity {
+    employee_name: string;
+    external_id: number;
+    personal_tax_id: string; // O CPF
+}
+
+export interface AttributeEntity {
+    instanceid: number;
+    name_filed: string;
+    value_field: string;
+}
+
+
+export interface ProcessStepEntity {
+    id: number;
+    instance_id: number;
+    requestname: string;
+    department_id: string;
+    requester_setor: string;
+    requester_setor_id: number;
+    requester_cargo: string;
+    requester_cargo_id: number;
+    flow_name: string;
+    task_instance_id: number;
+    task_id: number;
+    task_name: string;
+    task_type: string;
+    task_result: string;
+    task_startdatetime: Date;
+    task_enddatetime: Date;
+    executor_userid: number;
+    executor_name: string;
+    executor_email: string;
+    executor_username: string;
+    reportlink: string;
+}
